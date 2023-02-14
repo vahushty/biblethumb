aafcce823b60aa8eb97c6031bf47679efc5a8492
Vahushty <juarinio228@gmail.com>
2023-02-14 21:51:53 +0300

    Savimsya pocani

--- Init.js
+++ Init.js
@@ -14,27 +14,12 @@ async function initBibleApi() {
     });
   };
   let bibleApi = new BibleApi(initToken?.token, createToken);
-
-  // if (initToken === null) {
-  //   const data = await vizov.getInit();
-
-  //   if (data.msg !== undefined) {
-  //     console.log(data.msg);
-  //   } else {
-  //     initToken = data.token;
-  //     await prisma.baseInit.create({
-  //       data: {
-  //         id: 1,
-  //         token: initToken,
-  //       },
-  //     });
-  //     console.log(`We have got a new token here! ${initToken}`);
-  //   }
-  // } else {
-  //   console.log(`We have got an old token here! ${initToken.token}`);
-  //   initToken = initToken.token;
-  // }
-
   return bibleApi;
 }
 export const bibleApi = await initBibleApi();
+
+// class BibleInit extends BibleApi {
+//   async #initToken (){
+//     super.#initToken
+//   }
+// }
